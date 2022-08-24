d3e2fb0daba8156828cb27dae1754976f603a6b9
SergeMaikere <serge.maikere@gmail.com>
2022-08-24 18:33:29 +0200

    + web3 added to get uploader balance 	+ test for tipping video uploader added

--- truffle/test/DVideoTest.js
+++ truffle/test/DVideoTest.js
@@ -33,7 +33,7 @@ contract(
         )
 
         describe(
-            'upload', async () => {
+            'upload and tip', async () => {
                 const hash = '789456123';
                 const title = 'The 4 pallbearers';
                 const description = "What I wish my burial to look like";
@@ -75,8 +75,45 @@ contract(
                         assert.equal(video.id.toNumber(), videoCount.toNumber(), 'id is correct');
                         assert.equal(video.title, title, 'title is correct');
                         assert.equal(video.description, description, 'description is correct');
-                        assert.equal(video.tips.toNumber(), 0, 'tips is correct' );
+                        assert.equal(video.tips, 0, 'tips is correct' );
                         assert.equal(video.author, author, 'author is correct');
+                    }
+                )
+
+                it(
+                    'tips video uploader', async () => {
+                        //Get old author balance
+                        let oldBalance = await web3.eth.getBalance(author);
+                        oldBalance = web3.utils.BN(oldBalance);
+
+                        //Tip 1 Ether to author
+                        results = await dvideo.tipVideoOwner( videoCount, {from: tipper, value: web3.utils.toWei('1', 'Ether')} );
+
+                        //Get event
+                        event = results.logs[0].args;                        //Assert event's data is correct
+                        assert.equal(event.id.toNumber(), videoCount.toNumber(), 'id is correct');
+                        assert.equal(event.title, title, 'title is correct');
+                        assert.equal(event.description, description, 'description is correct');
+                        assert.equal(event.tips, 1000000000000000000, 'tips is correct' );
+                        assert.equal(event.author, author, 'author is ocrrect');
+
+                        //Get new author balance
+                        let newBalance = await web3.eth.getBalance(author);
+                        newBalance = web3.utils.BN(newBalance);
+                        
+                        //Set expected balance (old balance + tipeed amount)
+                        let tippedAmount = web3.utils.toWei('1', 'Ether');
+                        tippedAmount = web3.utils.BN(tippedAmount);
+                        const expectedBalance = oldBalance.add(tippedAmount);
+
+                        //Assert new balance and expected balance is correct
+                        assert.equal(newBalance.toString(), expectedBalance.toString(), 'balance is correct');
+                    }
+                )
+
+                it(
+                    'failed to tip video uploader', async () => {
+                        await dvideo.tipVideoOwner( 99, {from: tipper, value: web3.utils.toWei('1', 'Ether')} ).should.be.rejected;
                     }
                 )
             }
