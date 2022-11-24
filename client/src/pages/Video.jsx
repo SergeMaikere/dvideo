74475e3a252c07c0913fb699510566843f3b4584
SergeMaikere <serge.maikere@gmail.com>
2022-11-24 19:53:17 +0100

    hooks upgrade

--- client/src/pages/Video.jsx
+++ client/src/pages/Video.jsx
@@ -1,7 +1,7 @@
 import React, { useState, useEffect } from 'react';
 import { useParams } from 'react-router-dom';
 import { getVideo } from '../utils/Helper';
-import ReactPlayer from 'react-player';
+import Container from '@mui/material/Container';
 import Paper from '@mui/material/Paper';
 import Typography from '@mui/material/Typography';
 
@@ -15,26 +15,45 @@ const Video = props => {
     const [ createdAt, setCreatedAt ] = useState('');
     const { videoId } = useParams();
 
+    const addView = async () => {
+        await props.contract.methods.incrementViews(Number(videoId)).call();
+        console.log('addView');
+    }
+
     useEffect(
         () => {
             const init = async () => {
-                const video = await getVideo(props.contract, videoId);
+                const video = await getVideo(props.contract, Number(videoId));
+
+                if (!video) return;
+
+                await props.contract.events.VideoViewed( 
+                    {}, 
+                    (error, data) => {
+                        if (error)console.log('error', error) 
+                        if (data)console.log('data', data) 
+                    }
+                )
+
                 setTitle(video.title);
                 setDescription(video.description);
                 setHash(video.hash);
                 setChannelName(video.channelName);
                 setViews(video.views);
                 setCreatedAt(video.created_at);
+
             }
             init();
         }, [props.contract]
     )    
 
     return (
-        <Paper elevation={1}>
-            <Typography component="h4" aligh="center">{title}</Typography>
-            <video src={`http://localhost:8080/ipfs/${hash}`} controls></video>
-        </Paper>
+        <Container>
+            <Paper sx={{maxWidth: '600px'}} elevation={1}>
+                <Typography component="h4" align="center">{title}</Typography>
+                <video src={`http://localhost:8080/ipfs/${hash}`} controls onPlay={addView}></video>
+            </Paper>
+        </Container>
     );
 };
 
